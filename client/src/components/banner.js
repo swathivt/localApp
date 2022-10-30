7eaae751b8630af064493ad024519dc0908a361b
Swathy V <svanc212@mail.com>
2022-10-30 01:29:08 -0700

    Banner added

---
+++ client/src/components/banner.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export class Banner extends React.Component {
+  render() {
+    return (
+      <div id = "banner">
+        <img src="images/community.webp" />
+      </div>
+    );
+  }
+}
--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -3,14 +3,18 @@ import axios from "axios";
 import { NavBar } from "../components/sections/navBar.js";
 import { Footer } from "../components/sections/footer.js";
 import { Restaurants } from "../components/restaurants.js";
+import { Banner } from "../components/banner.js";
+
 
 export class Home extends React.Component {
   render() {
     return (
       <div id="pageContainer">
         <NavBar />
+        <Banner />
         <div className="container">
-          <Restaurants />
+          {/* <Restaurants /> */}
+          
         </div>
         <Footer />
       </div>
